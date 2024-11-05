49d817b49dbeb3ab9f403c7dfca7be539ee8f144
Prasadraju99 <evaraprasadraju.azure@gmail.com>
2024-11-05 14:58:53 +0530

    expense-infra-dev

---
+++ 02-sg/varaibles.tf
@@ -0,0 +1,21 @@
+variable "project_name" {
+  default = "expense"
+}
+# calling all the varaibles from module
+variable "environment" {
+  default = "dev"
+}
+
+variable "common_tags" {
+  default = {
+    Project     = "Expense"
+    Terraform   = "True"
+    Environment = "Dev"
+  }
+}
+
+variable "mysql_sg_tags" {
+  default = {
+    component = "mysql"
+  }
+}
