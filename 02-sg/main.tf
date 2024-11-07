d8529489066e1c51780374abdc4d505100fcb95c
Prasadraju99 <evaraprasadraju.azure@gmail.com>
2024-11-07 06:56:28 +0530

    expense-infra-dev

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -1,159 +1,159 @@
 module "mysql_sg" {
-  source = "../../terraform-aws-security-group"
+  source       = "../../terraform-aws-security-group"
   project_name = var.project_name
-  environment = var.environment
-  sg_name = "mysql"
-  vpc_id = local.vpc_id
-  common_tags = var.common_tags
-  sg_tags = var.mysql_sg_tags
+  environment  = var.environment
+  sg_name      = "mysql"
+  vpc_id       = local.vpc_id
+  common_tags  = var.common_tags
+  sg_tags      = var.mysql_sg_tags
 }
 
 module "backend_sg" {
-  source = "../../terraform-aws-security-group"
+  source       = "../../terraform-aws-security-group"
   project_name = var.project_name
-  environment = var.environment
-  sg_name = "backend"
-  vpc_id = local.vpc_id
-  common_tags = var.common_tags
-  sg_tags = var.backend_sg_tags
+  environment  = var.environment
+  sg_name      = "backend"
+  vpc_id       = local.vpc_id
+  common_tags  = var.common_tags
+  sg_tags      = var.backend_sg_tags
 }
 
 module "frontend_sg" {
-  source = "../../terraform-aws-security-group"
+  source       = "../../terraform-aws-security-group"
   project_name = var.project_name
-  environment = var.environment
-  sg_name = "frontend"
-  vpc_id = local.vpc_id
-  common_tags = var.common_tags
-  sg_tags = var.frontend_sg_tags
+  environment  = var.environment
+  sg_name      = "frontend"
+  vpc_id       = local.vpc_id
+  common_tags  = var.common_tags
+  sg_tags      = var.frontend_sg_tags
 }
 
 module "bastion_sg" {
-  source = "../../terraform-aws-security-group"
+  source       = "../../terraform-aws-security-group"
   project_name = var.project_name
-  environment = var.environment
-  sg_name = "bastion"
-  vpc_id = local.vpc_id
-  common_tags = var.common_tags
-  sg_tags = var.bastion_sg_tags
+  environment  = var.environment
+  sg_name      = "bastion"
+  vpc_id       = local.vpc_id
+  common_tags  = var.common_tags
+  sg_tags      = var.bastion_sg_tags
 }
 
 module "ansible_sg" {
-  source = "../../terraform-aws-security-group"
+  source       = "../../terraform-aws-security-group"
   project_name = var.project_name
-  environment = var.environment
-  sg_name = "ansible"
-  vpc_id = local.vpc_id
-  common_tags = var.common_tags
-  sg_tags = var.ansible_sg_tags
+  environment  = var.environment
+  sg_name      = "ansible"
+  vpc_id       = local.vpc_id
+  common_tags  = var.common_tags
+  sg_tags      = var.ansible_sg_tags
 }
 
 # MYSQL allowing connection on 3306 from the instances attached to backend security group
 resource "aws_security_group_rule" "mysql_backend" {
-  type              = "ingress"
-  from_port         = 3306
-  to_port           = 3306
-  protocol          = "tcp"
-  source_security_group_id       = module.backend_sg.id
-  security_group_id = module.mysql_sg.id
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = module.backend_sg.id
+  security_group_id        = module.mysql_sg.id
 }
 
 # Backend allowing connection on 8080 from the instances attached to frontend security group
 resource "aws_security_group_rule" "backend_frontend" {
-  type              = "ingress"
-  from_port         = 8080
-  to_port           = 8080
-  protocol          = "tcp"
-  source_security_group_id       = module.frontend_sg.id
-  security_group_id = module.backend_sg.id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  source_security_group_id = module.frontend_sg.id
+  security_group_id        = module.backend_sg.id
 }
 
 # Frontend allowing connection on 80 from the instances to access by public
 resource "aws_security_group_rule" "frontend_public" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  source_security_group_id       = ["0.0.0.0/0"]
-  security_group_id = module.frontend_sg.id
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = ["0.0.0.0/0"]
+  security_group_id        = module.frontend_sg.id
 }
 
 # Bastion allowing connection on 22 from the instances to access by mysql
 resource "aws_security_group_rule" "mysql_bastion" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  source_security_group_id       = module.bastion_sg.id
-  security_group_id = module.mysql_sg.id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = module.bastion_sg.id
+  security_group_id        = module.mysql_sg.id
 }
 
 # Bastion allowing connection on 22 from the instances to access by backend
 resource "aws_security_group_rule" "backend_bastion" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  source_security_group_id       = module.bastion_sg.id
-  security_group_id = module.backend_sg.id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = module.bastion_sg.id
+  security_group_id        = module.backend_sg.id
 }
 
 # Bastion allowing connection on 22 from the instances to access by frontend
 resource "aws_security_group_rule" "frontend_bastion" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  source_security_group_id       = module.bastion_sg.id
-  security_group_id = module.frontend_sg.id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = module.bastion_sg.id
+  security_group_id        = module.frontend_sg.id
 }
 
 # ansible allowing connection on 22 from the instances to access by mysql
 resource "aws_security_group_rule" "mysql_ansible" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  source_security_group_id       = module.ansible_sg.id
-  security_group_id = module.mysql_sg.id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = module.ansible_sg.id
+  security_group_id        = module.mysql_sg.id
 }
 
 # ansible allowing connection on 22 from the instances to access by backend
 resource "aws_security_group_rule" "backend_ansible" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  source_security_group_id       = module.ansible_sg_sg.id
-  security_group_id = module.backend_sg.id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = module.ansible_sg_sg.id
+  security_group_id        = module.backend_sg.id
 }
 
 # ansible allowing connection on 22 from the instances to access by frontend
 resource "aws_security_group_rule" "frontend_ansible" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  source_security_group_id       = module.ansible_sg.id
-  security_group_id = module.frontend_sg.id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = module.ansible_sg.id
+  security_group_id        = module.frontend_sg.id
 }
 
 # ansible allowing connection on 22 from the instances to access by users
 resource "aws_security_group_rule" "ansible_public" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  source_security_group_id       = ["0.0.0.0/0"]
-  security_group_id = module.ansible_sg.id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = ["0.0.0.0/0"]
+  security_group_id        = module.ansible_sg.id
 }
 
 # bastion allowing connection on 22 from the instances to access by users
 resource "aws_security_group_rule" "bastion_public" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  source_security_group_id       = ["0.0.0.0/0"]
-  security_group_id = module.bastion_sg.id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = ["0.0.0.0/0"]
+  security_group_id        = module.bastion_sg.id
 }
